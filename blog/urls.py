c6f196f4820587f7c076519123780cc11f699102
ripkoi123 <rogper94@ymail.com>
2019-05-27 17:17:55 -0500

    Second Try
    Signed-off-by: ripkoi123 <rogper94@ymail.com>

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+from django_filters.views import FilterView
+from blog.filters import PostFilter
 from . import views
 
 urlpatterns = [
@@ -8,6 +10,6 @@ urlpatterns = [
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete', PostDeleteView.as_view(), name='post-delete'),
-    path('search/', views.search, name='search'),
+    path('search/', FilterView.as_view(filterset_class=PostFilter, template_name='blog/search.html'), name='search'),
     path('about/', views.about, name='blog-about'),
 ]
